2aca72c3d6f4459f5e562d0dde9b03e39dbbd620
Nycollas <nhhenich@gmail.com>
2024-12-12 20:49:13 -0300

    gpt ex02

--- exercises_gpt/ex.rb
+++ exercises_gpt/ex.rb
@@ -2,6 +2,8 @@ puts 'Qual o seu nome?'
 name = gets.chomp
 
 puts "How old are you?"
-old = gets.chomp
+old = gets.chomp.to_i # tranforma o numero em integger
+
+puts old.class # mostra a classe
 
 puts "Olá #{name}, you are a #{old} years."
---
+++ exercises_gpt/ex02.rb
@@ -0,0 +1,7 @@
+puts "Informe um número"
+number_1 = gets.chomp.to_i
+
+puts "Informe um segundo número"
+number_2 = gets.chomp.to_i
+
+puts "#{number_1} + #{number_2} = #{number_1 + number_2}"
