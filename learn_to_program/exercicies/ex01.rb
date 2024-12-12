e0723a15a54ebbcf12002394a5253f26986c7482
Nycollas <nhhenich@gmail.com>
2024-12-11 21:37:23 -0300

    first exercise

---
+++ learn_to_program/exercicies/ex01.rb
@@ -0,0 +1,14 @@
+# Quantas horas tem um ano?
+
+day = 24
+hour = 60.0
+
+minute_on_day = day * hour
+
+day_month = 30
+minute_on_month = minute_on_day * day_month
+
+month_year = 12
+minute_on_year = minute_on_month * month_year
+puts "In the year to #{minute_on_year} minutes! (counting 30 days)."
+
