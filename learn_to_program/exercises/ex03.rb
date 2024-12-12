c5cd22ecd1292086eee3c05ae5c84666c1ddd023
Nycollas <nhhenich@gmail.com>
2024-12-11 21:55:52 -0300

    third exercise

---
+++ learn_to_program/exercises/ex03.rb
@@ -0,0 +1,12 @@
+# Quantos segundos você tem?
+
+year_of_birth = 2006
+current_year = 2024
+
+second_in_year = 31536000
+
+year_old = current_year - year_of_birth
+
+seconds_in_life = second_in_year * year_old
+
+puts "You are #{year_old} years old and #{seconds_in_life} seconds old."
