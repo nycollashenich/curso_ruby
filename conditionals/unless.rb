10ae1d5a2042df3408c55d847347af7c2befbf3f
Nycollas <nhhenich@gmail.com>
2024-12-16 12:58:52 -0300

    unless

---
+++ conditionals/unless.rb
@@ -0,0 +1,13 @@
+# oposto do if
+# somente é exexutado se a condição for false
+
+login = false
+
+unless login
+  
+  puts 'você precisa fazer login'
+
+end
+
+puts 'Faça login para continuar' unless login
+
