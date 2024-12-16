8c671eaf5a52c87a689771eb73ea375eca10cd76
Nycollas <nhhenich@gmail.com>
2024-12-16 12:52:04 -0300

    if elsif else

---
+++ conditionals/if_elsif_else.rb
@@ -0,0 +1,20 @@
+# if condicao
+
+# elsif outra_condicao
+
+# else
+
+# end
+
+ano = 2001
+
+if ano == 2000
+    puts "Ano 2000"
+
+elsif ano < 2000
+    puts 'Ano é menor que 2000'
+
+else
+    puts 'Ano é maior que 2000'
+
+end
