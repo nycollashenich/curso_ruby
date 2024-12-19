dc97be515efd9a96e8b0c2b22840908095bf171c
Nycollas <nhhenich@gmail.com>
2024-12-19 18:59:37 -0300

    POO, require and require relative

---
+++ POOII/animal.rb
@@ -0,0 +1,8 @@
+class Animal
+  def pular
+    puts 'Toin'
+  end
+  def dormir
+    puts 'Zzzzzzz'
+  end
+end
---
+++ POOII/app.rb
@@ -0,0 +1,11 @@
+require_relative 'C:/Users/nhhen/OneDrive/Área de Trabalho/ruby/POOII/animal'
+require_relative 'C:/Users/nhhen/OneDrive/Área de Trabalho/ruby/POOII/cachorro'
+
+
+animal = Animal.new
+animal.pular
+animal.dormir
+
+puts 'Animal'
+cachorro = Cachorro.new
+cachorro.latir
